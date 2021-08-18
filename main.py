071f8b6aae6b46e6cd0a399861375e7d6862a44d
Zach Wingrave <github@zachwingrave.com>
2021-08-18 23:59:12 +1000

    Algorithm to find index for any number in a given base

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from string import ascii_letters
+from string import digits
+from string import punctuation
+import itertools
+
+ALPHABET = ascii_letters + digits + punctuation
 
 def power(n, m):
   """ Return base 'n' to power of exponent 'm' """
@@ -5,6 +11,27 @@ def power(n, m):
     return 1
   else:
     return n*power(n, m-1)
+
+def find_position(word, alphabet=ALPHABET):
+  current_power = len(word) - 1
+  base = len(ALPHABET)
+  pos = 0
+  for char in word:
+    char_pos = alphabet.find(char) + 1
+    pos += char_pos * power(base, current_power)
+    current_power -= 1
+  return pos
+
+def test_find_pos(word, alphabet=ALPHABET):
+  counter = 1
+  for length in range(1, len(word) + 1):
+    wordlist = itertools.product(alphabet, repeat=length)
+    for word in wordlist:
+      if find_position(word) != counter:
+        print('Error at position', counter)
+        return False
+      counter += 1
+  return True
 
 def percentage_difference_a(n, m):
   """ Return percentage difference of 'n' over 'm' """
